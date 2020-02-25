a192bd6fcb95ef749b9ea6c320fb72544332f756
Ophelie Raymond Duhamel <ophe.duhamel@gmail.com>
2020-02-25 12:09:51 +0100

    As a user, I can add a new task

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   get 'tasks',     to: 'tasks#index'
   # Create a new task
   get 'tasks/new', to: 'tasks#new', as: :new_task
+  post 'tasks', to: 'tasks#create'
   # Read (one)
   get 'tasks/:id', to: 'tasks#show', as: :task
 end
