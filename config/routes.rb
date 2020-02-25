74d80f8681f49b710859748b25d2a89cdb199e94
Ophelie Raymond Duhamel <ophe.duhamel@gmail.com>
2020-02-25 12:32:24 +0100

    Implemented:  As a user, I can remove a task

--- config/routes.rb
+++ config/routes.rb
@@ -4,12 +4,18 @@ Rails.application.routes.draw do
   # Task CRUD
   # Read (all)
   get 'tasks',     to: 'tasks#index'
+
   # Create a new task
   get 'tasks/new', to: 'tasks#new', as: :new_task
   post 'tasks', to: 'tasks#create'
+
+  # Read (one)
+  get 'tasks/:id', to: 'tasks#show', as: :task
+
   # Update
   get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
   patch 'tasks/:id', to: 'tasks#update'
-  # Read (one)
-  get 'tasks/:id', to: 'tasks#show', as: :task
+
+  # Delete
+  delete 'tasks/:id', to: 'tasks#destroy'
 end
