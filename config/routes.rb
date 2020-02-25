053cb1b303516077c1102f7e5b36a41867be7a6e
Ophelie Raymond Duhamel <ophe.duhamel@gmail.com>
2020-02-25 11:45:04 +0100

    Implemented: As a user, I can view the details of a task

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   # Task CRUD
-  get 'tasks', to: 'tasks#index'
+  # Read (all)
+  get 'tasks',     to: 'tasks#index'
+  # Read (one)
+  get 'tasks/:id', to: 'tasks#show', as: :task
 end
