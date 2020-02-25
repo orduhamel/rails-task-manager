0746139c8adf7756a4f26d2333c7a24485a605ff
Ophelie Raymond Duhamel <ophe.duhamel@gmail.com>
2020-02-25 12:39:43 +0100

    refacto done

--- config/routes.rb
+++ config/routes.rb
@@ -3,19 +3,20 @@ Rails.application.routes.draw do
 
   # Task CRUD
   # Read (all)
-  get 'tasks',     to: 'tasks#index'
+  # get 'tasks',     to: 'tasks#index'
 
-  # Create a new task
-  get 'tasks/new', to: 'tasks#new', as: :new_task
-  post 'tasks', to: 'tasks#create'
+  # # Create a new task
+  # get 'tasks/new', to: 'tasks#new', as: :new_task
+  # post 'tasks', to: 'tasks#create'
 
-  # Read (one)
-  get 'tasks/:id', to: 'tasks#show', as: :task
+  # # Read (one)
+  # get 'tasks/:id', to: 'tasks#show', as: :task
 
-  # Update
-  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
-  patch 'tasks/:id', to: 'tasks#update'
+  # # Update
+  # get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  # patch 'tasks/:id', to: 'tasks#update'
 
-  # Delete
-  delete 'tasks/:id', to: 'tasks#destroy'
+  # # Delete
+  # delete 'tasks/:id', to: 'tasks#destroy'
+  resources :tasks
 end
